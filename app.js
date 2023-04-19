47a6b63d9de55882794982e9ae7b0c07c9c3e265
Punit Kumar Saini <64743687+punit-saini@users.noreply.github.com>
2023-04-19 09:00:30 +0530

    improved app.listen

--- app.js
+++ app.js
@@ -373,8 +373,16 @@ async function filmFinder(){
 // console.log()
 
 
+const port = process.env.PORT || 8080
 
-app.listen( process.env.PORT || 3000, console.log('Listening on port 3000' ));
+    app.listen(port, (err, res) => {
+        if (err) {
+            console.log(err)
+            return res.status(500).send(err.message)
+        } else {
+            console.log('[INFO] Server Running on port:', port)
+        }
+    })
 
 
 
